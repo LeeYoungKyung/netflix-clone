04d4bc3cf965963196472bdd18a4bc5c4cca1859
LeeYoungKyung <128415146+LeeYoungKyung@users.noreply.github.com>
2024-03-19 12:10:25 +0900

    nav

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -4,21 +4,34 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Outlet } from 'react-router-dom';
+import './AppLayout.css';
+
 const Applayout = () => {
   return (
     <>
-      <Navbar expand='lg' className='bg-body-tertiary'>
-        <Container fluid>
-          <Navbar.Brand href='#'>Navbar scroll</Navbar.Brand>
+      <Navbar expand='lg' className='bg-body-tertiary '>
+        <Container fluid className='bg-black' style={{ height: '70px' }}>
+          <Navbar.Brand
+            href='/'
+            className='navbar-logo'
+            style={{ color: 'white' }}
+          >
+            logo
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls='navbarScroll' />
           <Navbar.Collapse id='navbarScroll'>
             <Nav
-              className='me-auto my-2 my-lg-0'
+              className='me-auto my-2 my-lg-0  '
               style={{ maxHeight: '100px' }}
               navbarScroll
             >
-              <Nav.Link href='#action1'>Home</Nav.Link>
-              <Nav.Link href='#action2'>Link</Nav.Link>
+              <Nav.Link href='/' className=' text-white'>
+                Home
+              </Nav.Link>
+              <Nav.Link href='/movies' className=' text-white'>
+                Movie
+              </Nav.Link>
             </Nav>
             <Form className='d-flex'>
               <Form.Control
@@ -27,13 +40,13 @@ const Applayout = () => {
                 className='me-2'
                 aria-label='Search'
               />
-              <Button variant='outline-success'>Search</Button>
+              <Button variant='outline-success '>search</Button>
             </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
-      {/*  */}
+      <Outlet />
     </>
   );
 };
