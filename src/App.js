d2ce4a4b0bdcc3f390e72f7a7a37a678d0638110
LeeYoungKyung <128415146+LeeYoungKyung@users.noreply.github.com>
2024-03-19 09:39:07 +0900

    Route 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
+// Importing components
+import AppLayout from './layout/AppLayout';
+import Homepage from './pages/Homepage/Homepage';
+import MoviePage from './pages/MovieDetail/MoviePage';
+import MovieDetailPage from './pages/Movies/MovieDetailPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  return <div>ffff</div>;
+  return (
+    <Routes>
+      <Route path='/' element={<AppLayout />}>
+        <Route index element={<Homepage />} />
+        <Route path='movies' element={<MoviePage />} />
+        <Route path='movies/:id' element={<MovieDetailPage />} />
+      </Route>
+      <Route path='*' element={<NotFoundPage />} />
+    </Routes>
+  );
 }
 
 export default App;
