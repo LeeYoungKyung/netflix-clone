ba29d2a339ae34df6ae3e5b4fd6fada027dfa631
LeeYoungKyung <128415146+LeeYoungKyung@users.noreply.github.com>
2024-05-23 16:21:44 +0900

    [feat] 404page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage 404</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: 'black',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        color: 'white',
+      }}
+    >
+      죄송합니다. 현재 찾을 . 수없는 페이지를 요청하셨습니다 <br /> <br />
+      존재하지 않는 주소를 입력하셨거나
+      <br /> <br />
+      요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다
+      <br /> <br />
+      궁금한 점이 있으시다면 언제든 https://github.com/LeeYoungKyung 에 문의해
+      주시기 바랍니다
+      <br /> <br />
+      감사합니다
+      <br /> <br />
+    </div>
+  );
 };
 
 export default NotFoundPage;
